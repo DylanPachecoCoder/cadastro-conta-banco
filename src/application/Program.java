c1059957b06773344c8334c7af81e613c4dc3c33
Dylan Pacheco de Castro Lima <dylanpacheco@brq.com>
2019-09-03 13:42:17 -0300

    Criando menu principal

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,24 +1,29 @@
 package application;
 
+import java.util.Scanner;
+
 import entities.CheckingAccount;
 import entities.SavingsAccount;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
-		CheckingAccount checkingAccount = new CheckingAccount(111, 111);
+
+		UI.displayMenu();
+	
+		/*CheckingAccount checkingAccount = new CheckingAccount(111, 111);
 		checkingAccount.deposit(100);
-		
+
 		SavingsAccount savingsAccount = new SavingsAccount(222, 222);
 		savingsAccount.deposit(200);
-		
+
 		checkingAccount.transfer(10, savingsAccount);
-		
+
 		System.out.println("CA: " + checkingAccount.getBalance());
-		
-		System.out.println("SA: " + savingsAccount.getBalance());
-		
+
+		System.out.println("SA: " + savingsAccount.getBalance());*/
+
+
 	}
-	
+
 }
